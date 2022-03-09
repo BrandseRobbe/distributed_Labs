7967edf6eb6e4bb624612d2ace961755f03acb13
SeppeVdP <70763433+SeppeVdP@users.noreply.github.com>
2022-03-09 11:54:21 +0100

    toberolledback

--- src/main/java/helloWorld.java
+++ src/main/java/helloWorld.java
@@ -1,6 +1,9 @@
+package src.main.java;
 
 public class helloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+        System.out.println("toberolledback");
     }
 }
